290161262907a53450227e4b25851912fc5d5540
amantarento <amankumar.sharma@tarento.com>
2021-10-13 17:11:23 +0530

    verify the oauth token and call the lms api to create user

--- src/publicApi_v8/googleSignInRoutes.ts
+++ src/publicApi_v8/googleSignInRoutes.ts
@@ -6,8 +6,9 @@ import { axiosRequestConfig } from '../configs/request.config'
 import { CONSTANTS } from '../utils/env'
 import { logError, logInfo } from '../utils/logger'
 const API_END_POINTS = {
+    createUserWithMailId: `${CONSTANTS.LEARNER_SERVICE_API_BASE}/user/v2/signup`,
    fetchUserByEmailId: `${CONSTANTS.LEARNER_SERVICE_API_BASE}/user/v1/exists/email`,
-   createUserWithMailId: `${CONSTANTS.LEARNER_SERVICE_API_BASE}/user/v2/signup`,
+
 }
 const client = new OAuth2Client(CONSTANTS.GOOGLE_CLIENT_ID)
 export const googleAuth = Router()
@@ -17,18 +18,21 @@ googleAuth.get('/google/auth/callback', async (req) => {
     try {
         const { idToken } = req.body
         await client.verifyIdToken({
-            idToken,
             audience: CONSTANTS.GOOGLE_CLIENT_ID,
+            idToken,
           }).then((response) => {
-            if (response.getPayload() && response.getPayload()?.email_verified) {
-                let googleProfile = {
-                    emailId : response.getPayload()?.email,
-                    name : response.getPayload() ?.name 
+            if (response.getPayload()) {
+                // tslint:disable-next-line: no-any
+                const data: any = response.getPayload()
+                // tslint:disable-next-line: no-any
+                const googleProfile: any = {
+                    emailId : data.email,
+                    name : data.name,
                 }
-                let isUserExist = fetchUserByEmailId(googleProfile.emailId)
+                const isUserExist = fetchUserByEmailId(googleProfile.emailId)
                 logInfo('sunbird profile fetched' + JSON.stringify(isUserExist))
                 if (!isUserExist) {
-                    createUserWithMailId(googleProfile, CONSTANTS.GOOGLE_CLIENT_ID)
+                    createUserwithMailId(googleProfile, CONSTANTS.GOOGLE_CLIENT_ID)
                     logInfo('google sign in success' + googleProfile)
                 }
             } else {
@@ -40,50 +44,52 @@ googleAuth.get('/google/auth/callback', async (req) => {
     }
 
 })
-
-const fetchUserByEmailId = async (emailId: any) => {
-    const response = await axios( {
-        ...axiosRequestConfig,
-         method: 'GET',
-         url: API_END_POINTS.fetchUserByEmailId + emailId,
-         headers: {
-           Authorization: CONSTANTS.SB_API_KEY,
-       },
-    })
-    if (response.data.responseCode === 'OK') {
-        return _.get(response, 'result.exists')
-    } else {
-        logError( 'fetchUserByEmailId failed')
-    }
-  }
-
-const createUserWithMailId = async (accountDetails: any, client_id: any) => {
+// tslint:disable-next-line: no-any
+const createUserwithMailId = async (accountDetails: any, client_id: string) => {
     if (!accountDetails.name || accountDetails.name === '') {
       throw new Error('USER_NAME_NOT_PRESENT')
     }
     const response = await axios( {
         ...axiosRequestConfig,
-        method: 'POST',
-        url: API_END_POINTS.createUserWithMailId,
         data: {
             body: {
             params: {
-              source: client_id,
               signupType: 'google',
+              source: client_id,
             },
             request: {
-              firstName: accountDetails.name,
               email: accountDetails.emailId,
               emailVerified: true,
+              firstName: accountDetails.name,
+
             }},
-            },
-          headers: {
-            Authorization: CONSTANTS.SB_API_KEY,
-         },
+        },
+        headers: {
+                Authorization: CONSTANTS.SB_API_KEY,
+        },
+        method: 'POST',
+        url: API_END_POINTS.createUserWithMailId,
+
     })
     if (response.data.responseCode === 'OK') {
         return response.data
     } else {
         logError( 'createUserWithMailId failed')
+    }
+  }
+const fetchUserByEmailId = async (emailId: string) => {
+    const response = await axios( {
+        ...axiosRequestConfig,
+        headers: {
+            Authorization: CONSTANTS.SB_API_KEY,
+        },
+        method: 'GET',
+        url: API_END_POINTS.fetchUserByEmailId + emailId,
+
+    })
+    if (response.data.responseCode === 'OK') {
+        return _.get(response, 'result.exists')
+    } else {
+        logError( 'fetchUserByEmailId failed')
     }
   }
