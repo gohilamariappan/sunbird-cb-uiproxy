99c519675c89bee096fdc2a7446a3acee907cd9c
Saurav Verma <saurav.verma@tarento.com>
2021-10-18 18:49:06 +0530

    Forgot password Implemented

--- src/publicApi_v8/forgotPassword.ts
+++ src/publicApi_v8/forgotPassword.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { Router } from 'express'
 import { axiosRequestConfig } from '../configs/request.config'
-//import { extractUserToken } from '../utils/requestExtract'
 
 import { logError, logInfo } from '../utils/logger'
 
