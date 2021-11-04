5ca45715868088be9a7d1c823adec0a7425b9366
amantarento <amankumar.sharma@tarento.com>
2021-11-04 11:37:55 +0530

    put the check for fitb questionType

--- src/protectedApi_v8/assessment.ts
+++ src/protectedApi_v8/assessment.ts
@@ -1,71 +1,71 @@
-import axios from "axios";
-import { Router } from "express";
-import _ from "lodash";
-import { axiosRequestConfig } from "../configs/request.config";
-import { logError, logInfo } from "../utils/logger";
-export const assessmentApi = Router();
-assessmentApi.post("/get", async (req, res) => {
+import axios from 'axios'
+import { Router } from 'express'
+import _ from 'lodash'
+import { axiosRequestConfig } from '../configs/request.config'
+import { logError, logInfo } from '../utils/logger'
+export const assessmentApi = Router()
+assessmentApi.post('/get', async (req, res) => {
   try {
     if (!req.body.artifactUrl) {
       res.status(400).json({
-        msg: "artifact Url can not be empty",
-        status: "error",
+        msg: 'artifact Url can not be empty',
+        status: 'error',
         status_code: 400,
-      });
+      })
     }
-    const { artifactUrl } = req.body;
-    const assessmentData = await fetchAssessment(artifactUrl);
-    const formatedData = getFormatedResponse(assessmentData);
-    res.status(200).json({ data: formatedData });
-    logInfo("formatedData Data in JSON :", JSON.stringify(formatedData));
+    const { artifactUrl } = req.body
+    const assessmentData = await fetchAssessment(artifactUrl)
+    const formatedData = getFormatedResponse(assessmentData)
+    res.status(200).json({ data: formatedData })
+    logInfo('formatedData Data in JSON :', JSON.stringify(formatedData))
   } catch (err) {
     res.status(401).send({
-      error: "error while fetching assesment !!",
-    });
+      error: 'error while fetching assesment !!',
+    })
   }
-});
+})
 // tslint:disable-next-line: no-any
 const fetchAssessment = async (artifactUrl: string) => {
-  logInfo("Checking fetchAssessment : ", artifactUrl);
+  logInfo('Checking fetchAssessment : ', artifactUrl)
   try {
     const response = await axios({
       ...axiosRequestConfig,
-      method: "GET",
+      method: 'GET',
       url: artifactUrl,
-    });
-    logInfo("Response Data in JSON :", JSON.stringify(response.data));
+    })
+    logInfo('Response Data in JSON :', JSON.stringify(response.data))
     if (response.data.questions) {
-      logInfo("Response questions :", _.get(response, "data.questions"));
-      return _.get(response, "data.questions");
+      logInfo('Response questions :', _.get(response, 'data.questions'))
+      return _.get(response, 'data.questions')
     }
   } catch (err) {
-    logError("fetchAssement  failed");
+    logError('fetchAssement  failed')
   }
-};
+}
 // tslint:disable-next-line: no-any
 const getFormatedResponse = (questions: any) => {
   logInfo(
-    "Response of questions in formated method JSON :",
+    'Response of questions in formated method JSON :',
     JSON.stringify(questions)
-  );
+  )
   return _.forEach(questions, (qkey) => {
-    logInfo("inside for each");
-    logInfo("inside qkey ", qkey.questionType);
-    if (qkey.questionType === "mcq-sca" && qkey.options.length > 0) {
+    logInfo('inside for each')
+    logInfo('inside qkey ', qkey.questionType)
+    if (qkey.questionType === 'mcq-sca' && qkey.options.length > 0) {
       _.forEach(qkey.options, (optKey) => {
-        _.set(optKey, "isCorrect", "false");
-      });
+        _.set(optKey, 'isCorrect', 'false')
+      })
       // eslint-disable-next-line
-    } else if (qkey.questionType === "mtf" && qkey.options.length > 0) {
+    } else if (qkey.questionType === 'mtf' && qkey.options.length > 0) {
       _.forEach(qkey.options, (optKey) => {
-        _.set(optKey, "isCorrect", "false");
-        _.set(optKey, "match", "");
-      });
-    } else if (qkey.questionType === "fitb" && qkey.options.length > 0) {
+        _.set(optKey, 'isCorrect', 'false')
+        _.set(optKey, 'match', '')
+      })
+    } else if (qkey.questionType === 'fitb' && qkey.options.length > 0) {
       _.forEach(qkey.options, (optKey) => {
-        _.set(optKey, "isCorrect", "false");
-        _.set(optKey, "text", "");
-      });
+        _.set(optKey, 'isCorrect', 'false')
+        _.set(optKey, 'text', '')
+      })
     }
-  });
-};
+  })
+}
