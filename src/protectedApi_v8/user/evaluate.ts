63725427ce6eb3679a33a0b7df535157a9f7338b
amantarento <amankumar.sharma@tarento.com>
2021-11-08 17:54:11 +0530

    submit assessment api is completeed

--- src/protectedApi_v8/user/evaluate.ts
+++ src/protectedApi_v8/user/evaluate.ts
@@ -1,20 +1,20 @@
-import axios from 'axios'
-import { Router } from 'express'
-import { axiosRequestConfig } from '../../configs/request.config'
-import { CONSTANTS } from '../../utils/env'
-import { ERROR } from '../../utils/message'
-import {
-  extractUserIdFromRequest,
-  extractUserToken,
-} from '../../utils/requestExtract'
+import axios from "axios";
+import { Router } from "express";
+import { axiosRequestConfig } from "../../configs/request.config";
+import { CONSTANTS } from "../../utils/env";
+import { ERROR } from "../../utils/message";
+// import {
+//   extractUserIdFromRequest,
+//   extractUserToken,
+// } from '../../utils/requestExtract'
 
-const GENERAL_ERR_MSG = 'Failed due to unknown reason'
+const GENERAL_ERR_MSG = "Failed due to unknown reason";
 const API_END_POINTS = {
   assessmentSubmitV2: `${CONSTANTS.KONG_API_BASE}/v2/user`,
   iapSubmitAssessment: `${CONSTANTS.SB_EXT_API_BASE_2}/v3/iap-assessment`,
   postAssessment: `${CONSTANTS.POST_ASSESSMENT_BASE}/lmsapi/v1/post_assessment`,
-}
-export const evaluateApi = Router()
+};
+export const evaluateApi = Router();
 
 // evaluateApi.post('/assessment/submit/v2', async (req, res) => {
 //   try {
@@ -51,11 +51,11 @@ export const evaluateApi = Router()
 //   }
 // })
 
-evaluateApi.post('/assessment/submit/iap', async (req, res) => {
-  const url = `${API_END_POINTS.iapSubmitAssessment}`
+evaluateApi.post("/assessment/submit/iap", async (req, res) => {
+  const url = `${API_END_POINTS.iapSubmitAssessment}`;
   const requestBody = {
     ...req.body,
-  }
+  };
 
   axios({
     ...axiosRequestConfig,
@@ -63,11 +63,11 @@ evaluateApi.post('/assessment/submit/iap', async (req, res) => {
     headers: {
       rootOrg: requestBody.root_org,
     },
-    method: 'POST',
+    method: "POST",
     url,
   })
     .then((response) => {
-      res.status(response.status).send(response.data)
+      res.status(response.status).send(response.data);
     })
     .catch((error) => {
       res
@@ -76,22 +76,22 @@ evaluateApi.post('/assessment/submit/iap', async (req, res) => {
           (error && error.response && error.response.data) || {
             error: GENERAL_ERR_MSG,
           }
-        )
-    })
-})
+        );
+    });
+});
 
-evaluateApi.get('/post-assessment/:contentId', async (req, res) => {
-  const uuid = req.header('wid')
-  const contentId = req.params.contentId
-  const rootOrg = req.header('rootOrg')
+evaluateApi.get("/post-assessment/:contentId", async (req, res) => {
+  const uuid = req.header("wid");
+  const contentId = req.params.contentId;
+  const rootOrg = req.header("rootOrg");
   if (!rootOrg) {
-    res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
-    return
+    res.status(400).send(ERROR.ERROR_NO_ORG_DATA);
+    return;
   }
   const body = {
     contentIds: [contentId],
     userIds: [uuid],
-  }
+  };
   try {
     const response = await axios.post(API_END_POINTS.postAssessment, body, {
       ...axiosRequestConfig,
@@ -100,13 +100,13 @@ evaluateApi.get('/post-assessment/:contentId', async (req, res) => {
         client_secret: CONSTANTS.POST_ASSESSMENT_CLIENT_SECRET,
         rootOrg,
       },
-    })
-    res.send(response.data)
+    });
+    res.send(response.data);
   } catch (err) {
     res.status((err && err.response && err.response.status) || 500).send(
       (err && err.response && err.response.data) || {
         error: GENERAL_ERR_MSG,
       }
-    )
+    );
   }
-})
+});
