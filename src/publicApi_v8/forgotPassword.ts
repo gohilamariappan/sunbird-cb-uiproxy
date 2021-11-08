a1e3b9cecdb6a23868f81e01d8b1fff055f0ae16
Saurav Verma <saurav.verma@tarento.com>
2021-11-08 11:30:45 +0530

    forgot password

--- src/publicApi_v8/forgotPassword.ts
+++ src/publicApi_v8/forgotPassword.ts
@@ -1,65 +1,88 @@
 import axios from 'axios'
 import { Router } from 'express'
-import _ from 'lodash'
 import { axiosRequestConfig } from '../configs/request.config'
 import { CONSTANTS } from '../utils/env'
 import { logError, logInfo } from '../utils/logger'
+import { extractUserToken } from '../utils/requestExtract'
 
 const API_END_POINTS = {
-    fetchUserByEmailId: `${CONSTANTS.KONG_API_BASE}/user/v1/exists/email/`,
     kongSearchUser: `${CONSTANTS.KONG_API_BASE}/user/v1/search`,
     kongUserResetPassword: `${CONSTANTS.KONG_API_BASE}/private/user/v1/password/reset`,
   }
 
+const uuidv1            = require('uuid/v1')
+const dateFormat        = require('dateformat')
+const emailAdressExist = 'Email address already exist'
+
 export const forgotPassword = Router()
 
-forgotPassword.post('/verify', async (req, _res) => {
-    const sbemail = req.body.personalDetails.email
-    logInfo('Entered into forgot password and email is : ', sbemail)
-    logInfo('Checking Fetch email id value : ', API_END_POINTS.fetchUserByEmailId + sbemail)
-    logInfo('Google auth callback called' )
+forgotPassword.post('/verify', async (req, res) => {
+    logInfo('Entered into forgot password')
     try {
-     
-        logInfo('Entering into forgot password section : ', sbemail )
-        const isUserExist = await fetchUserByEmailId(sbemail)
-        if (isUserExist) {
-            logInfo('Email already exists.')
-            _res.status(400).json({ status: 'error' , status_code: 400, msg: 'Email already exists.' })
-        } else {
-            logInfo('Check !! User doesnot exists !! ')
-            
-                _res.status(200).json({ status: 'success', status_code: 200, msg: 'User doesnot exist!!'})
-           
-        }
-        return
-    } catch (err) {
-        _res.status(401).send('Token Expired !!')
-        logError('ERROR CREATING USER' + err )
-    }
-
-})
-
-
-const fetchUserByEmailId = async (emailId: string) => {
-    logInfo('Checking Fetch email id value : ', API_END_POINTS.fetchUserByEmailId + emailId)
-    try {
-        const response = await axios( {
+        const sbemail_ = req.body.personalDetails.email
+        logInfo('URL Passing : ', req.body.personalDetails.email)
+        const searchresponse = await axios({
             ...axiosRequestConfig,
+            data: { request: { query: '', filters: { email: sbemail_.toLowerCase() } } },
             headers: {
                 Authorization: CONSTANTS.SB_API_KEY,
+                // tslint:disable-next-line: all
+                'x-authenticated-user-token': extractUserToken(req)
             },
-            method: 'GET',
-            url: API_END_POINTS.fetchUserByEmailId + emailId,
-
+            method: 'POST',
+            url: API_END_POINTS.kongSearchUser,
         })
-        logInfo( 'Response Data in JSON :', JSON.stringify(response.data))
-        logInfo( 'Response Data in Success :', response.data.responseCode)
-        if (response.data.responseCode === 'OK') {
-            logInfo( 'Response result.exists :', _.get(response, 'data.result.exists'))
-            return _.get(response, 'data.result.exists')
-        }
-    } catch (err) {
-        logError( 'fetchUserByEmailId failed')
-    }
+        logInfo('Email Data : ', req.body.personalDetails.email)
 
-  }
+        if (searchresponse.data.result.response.count > 0) {
+            logInfo('Entered into Search Response')
+            res.status(200).send(
+            {
+                id: 'api.error.createUser',
+                ver: '1.0',
+                // tslint:disable-next-line: object-literal-sort-keys
+                ts: dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss:lo'),
+                params:
+                {
+                    resmsgid: uuidv1(),
+                    // tslint:disable-next-line: object-literal-sort-keys
+                    msgid: null,
+                    status: 'failed',
+                    err: 'USR_EMAIL_EXISTS',
+                    errmsg: emailAdressExist,
+                },
+                responseCode: 'USR_EMAIL_EXISTS',
+                result: {},
+            })
+            res.status(200).send('User search successful')
+            return
+        } else {
+            const sbUserId = searchresponse.data.result.userId
+            const passwordResetRequest = {
+                key: 'email',
+                type: 'email',
+                userId: sbUserId,
+            }
+
+            logInfo('Sending Password reset request -> ' + passwordResetRequest)
+            logInfo('User id -> ' + sbUserId)
+            const passwordResetResponse = await axios({
+                ...axiosRequestConfig,
+                data: { request: passwordResetRequest },
+                headers: {
+                    Authorization: CONSTANTS.SB_API_KEY,
+                },
+                method: 'POST',
+                url: API_END_POINTS.kongUserResetPassword,
+            })
+            logInfo('Received response from password reset -> ' + passwordResetResponse)
+            if (passwordResetResponse.data.params.status === 'success') {
+                logInfo('Reset password', passwordResetResponse.data.params.status)
+                res.status(200).send('User password reset successfully !!')
+            }
+        }
+
+    } catch (err) {
+        logError('Error failing the call : ' + err)
+    }
+})
