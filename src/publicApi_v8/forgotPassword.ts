6e91ede00501532a10339a4c189758f2dd55c5e0
Saurav Verma <saurav.verma@tarento.com>
2021-11-08 17:33:52 +0530

    forgot password implementation with private user

--- src/publicApi_v8/forgotPassword.ts
+++ src/publicApi_v8/forgotPassword.ts
@@ -1,62 +1,38 @@
+
 import axios from 'axios'
 import { Router } from 'express'
-import { axiosRequestConfig } from '../configs/request.config'
 import { CONSTANTS } from '../utils/env'
+import { axiosRequestConfig } from '../configs/request.config';
 import { logError, logInfo } from '../utils/logger'
-import { extractUserToken } from '../utils/requestExtract'
 
 const API_END_POINTS = {
-    kongSearchUser: `${CONSTANTS.KONG_API_BASE}/user/v1/search`,
-    kongUserResetPassword: `${CONSTANTS.KONG_API_BASE}/private/user/v1/password/reset`,
-  }
-
-const uuidv1            = require('uuid/v1')
-const dateFormat        = require('dateformat')
-const emailAdressExist = 'Email address already exist'
+                        searchSb: `${CONSTANTS.LEARNER_SERVICE_API_BASE}/private/user/v1/search`,
+                        }    
 
 export const forgotPassword = Router()
 
-forgotPassword.post('/verify', async (req, res) => {
-    logInfo('Entered into forgot password as')
+forgotPassword.post('/reset/proxy/password', async (req, res) => {
+    logInfo("Entered into reset password ") 
+    try
+    {
+        logInfo("Entered into try block ")
+        const sbemail = req.body.email
 
-    try {
-        const  sbemail  = req.body.email
-        logInfo('URL Passing : ', JSON.stringify(sbemail))
-
+        logInfo("Entered into try block email : ", sbemail)
+        
         const searchresponse = await axios({
             ...axiosRequestConfig,
             data: { request: { query: '', filters: { email: sbemail.toLowerCase() } } },
-            headers: {
-                Authorization: CONSTANTS.SB_API_KEY,
-                // tslint:disable-next-line: all
-                'x-authenticated-user-token': extractUserToken(req),
-            },
             method: 'POST',
-            url: API_END_POINTS.kongSearchUser,
+            url: API_END_POINTS.searchSb,
         })
-        logInfo('After hit received response is : ' + searchresponse)
-        res.status(200).send(
-            {
-                id: 'api.error.createUser',
-                ver: '1.0',
-                // tslint:disable-next-line: object-literal-sort-keys
-                ts: dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss:lo'),
-                params:
-                {
-                    resmsgid: uuidv1(),
-                    // tslint:disable-next-line: object-literal-sort-keys
-                    msgid: null,
-                    status: 'failed',
-                    err: 'USR_EMAIL_EXISTS',
-                    errmsg: emailAdressExist,
-                },
-                responseCode: 'USR_EMAIL_EXISTS',
-                result: {},
-            })
-        return
+        logInfo("Entered into reset password "+ searchresponse)    
+        res.status(200).send("Success in search User !! ")
+        return  
 
-    } catch (err) {
-        logError('Hey ! Its Error failing the call : ' + err)
-        res.status(400).send("Error throwing")
+    }catch (err) {
+        logError('ERROR CREATING USER REGISTRY > '+ err)
+        res.status(500).send("Error "+err) 
     }
-})
+})        
+
