efbf3935143119567dd661ab8dd44fb9035037c4
Saurav Verma <saurav.verma@tarento.com>
2021-11-09 11:00:41 +0530

    forgot password : getting usertype

--- src/publicApi_v8/forgotPassword.ts
+++ src/publicApi_v8/forgotPassword.ts
@@ -6,8 +6,8 @@ import { CONSTANTS } from '../utils/env'
 import { logError, logInfo } from '../utils/logger'
 
 const API_END_POINTS = {
-                        searchSb: `${CONSTANTS.LEARNER_SERVICE_API_BASE}/private/user/v1/search`,
                         resendOTP: `${CONSTANTS.MSG91BASE}/api/v5/otp/retry`,
+                        searchSb: `${CONSTANTS.LEARNER_SERVICE_API_BASE}/private/user/v1/search`,
                         sendOTP: `${CONSTANTS.MSG91BASE}/api/v5/otp`,
                         verifyOTP: `${CONSTANTS.MSG91BASE}/api/v5/otp/verify`,
                         }
@@ -15,7 +15,7 @@ const API_END_POINTS = {
 const msgKey = CONSTANTS.MSG91KEY
 // Routes
 // create account
-const MSGERROR = 'MSG91 ERROR'                        
+const MSGERROR = 'MSG91 ERROR'
 
 export const forgotPassword = Router()
 
@@ -35,18 +35,18 @@ forgotPassword.post('/reset/proxy/password', async (req, res) => {
         })
         if (searchresponse.data.result.response.count > 0) {
             logInfo('User found with user id : ', searchresponse.data.result.response.content.userId)
-           const userId =  _.get(_.find(searchresponse.data.result.response.content,'userId'), 'userId')
+            const userId =  _.get(_.find(searchresponse.data.result.response.content, 'userId'), 'userId')
           // const mobileNumber = req.body.mobileNumber
            // generate otp
-           const userType = await emailOrMobile(sbUsername)
-           //await sendOTP(mobileNumber)
-           logInfo("User type : ", userType)
-           logInfo("User Id : ", userId)
-           logInfo("UserName : ", sbUsername)
-           res.status(200).send(userId)
-           //res.status(200).json({message: 'Success'})
-           return
-            //res.status(200).send(userId)
+            const userType = await emailOrMobile(sbUsername)
+           // await sendOTP(mobileNumber)
+            logInfo('User type : ', userType)
+            logInfo('User Id : ', userId)
+            logInfo('UserName : ', sbUsername)
+            res.status(200).send(userId)
+           // res.status(200).json({message: 'Success'})
+            return
+            // res.status(200).send(userId)
 
         } else {
             logInfo('Couldnot find the user : ', searchresponse.data.result.response)
@@ -73,15 +73,15 @@ export function emailOrMobile(value: string) {
     return 'error'
   }
 
-  export function emailValidator(value: string) {
+export function emailValidator(value: string) {
     // tslint:disable-next-line: max-line-length
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)
   }
-  const mobileValidator = (value: string) => {
+const mobileValidator = (value: string) => {
     return /^([7-9][0-9]{9})$/.test(value)
   }
 
-  export async function sendOTP(mobileNumber: string) {
+export async function sendOTP(mobileNumber: string) {
     try {
       mobileNumber = '91' + mobileNumber
       const url = `${API_END_POINTS.sendOTP}?authkey=${msgKey}&template_id=${CONSTANTS.MSG91TEMPLATEID}&mobile=${mobileNumber}&invisible=1`
@@ -90,5 +90,5 @@ export function emailOrMobile(value: string) {
       logError(MSGERROR, err)
       return 'Error'
     }
-  
+
   }
