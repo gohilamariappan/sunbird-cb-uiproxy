17921cb2d734849047f064affa42152e031cf32a
amantarento <amankumar.sharma@tarento.com>
2021-11-26 11:48:56 +0530

    public content api is completed

--- src/publicApi_v8/publicApiV8.ts
+++ src/publicApi_v8/publicApiV8.ts
@@ -1,28 +1,35 @@
-import express from 'express'
-import { CONSTANTS } from '../utils/env'
-import { proxyCreatorRoute } from '../utils/proxyCreator'
-import { customSignUp } from './customSignup'
-import {emailOrMobileLogin} from './emailOrMobileLoginSignIn'
-import { forgotPassword } from './forgotPassword'
-import {googleAuth} from './googleSignInRoutes'
-import { homePage } from './home'
-import { signup } from './signup'
-import { publicTnc } from './tnc'
-export const publicApiV8 = express.Router()
+import express from "express";
+import { CONSTANTS } from "../utils/env";
+import { proxyCreatorRoute } from "../utils/proxyCreator";
+import { customSignUp } from "./customSignup";
+import { emailOrMobileLogin } from "./emailOrMobileLoginSignIn";
+import { forgotPassword } from "./forgotPassword";
+import { googleAuth } from "./googleSignInRoutes";
+import { homePage } from "./home";
+import { signup } from "./signup";
+import { publicTnc } from "./tnc";
+import { publicContentApi } from "./publicContent";
+export const publicApiV8 = express.Router();
 
-publicApiV8.get('/', (_req, res) => {
+publicApiV8.get("/", (_req, res) => {
   res.json({
     status: `Public Api is working fine https base: ${CONSTANTS.HTTPS_HOST}`,
-  })
-})
+  });
+});
 
-publicApiV8.use('/assets',
-  proxyCreatorRoute(express.Router(), CONSTANTS.WEB_HOST_PROXY + '/web-hosted/web-client-public-assets'))
+publicApiV8.use(
+  "/assets",
+  proxyCreatorRoute(
+    express.Router(),
+    CONSTANTS.WEB_HOST_PROXY + "/web-hosted/web-client-public-assets"
+  )
+);
 
-publicApiV8.use('/tnc', publicTnc)
-publicApiV8.use('/signup', signup)
-publicApiV8.use('/homePage', homePage)
-publicApiV8.use('/register/', customSignUp)
-publicApiV8.use('/emailMobile/', emailOrMobileLogin)
-publicApiV8.use('/google/', googleAuth)
-publicApiV8.use('/forgot-password/', forgotPassword)
+publicApiV8.use("/tnc", publicTnc);
+publicApiV8.use("/signup", signup);
+publicApiV8.use("/homePage", homePage);
+publicApiV8.use("/register/", customSignUp);
+publicApiV8.use("/emailMobile/", emailOrMobileLogin);
+publicApiV8.use("/google/", googleAuth);
+publicApiV8.use("/forgot-password/", forgotPassword);
+publicApiV8.use("/publicContent/", publicContentApi);
---
+++ src/publicApi_v8/publicContent.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { Router } from "express";
+import { axiosRequestConfig } from "../configs/request.config";
+import { IContent } from "../models/content.model";
+import { processContent } from "../utils/contentHelpers";
+import { CONSTANTS } from "../utils/env";
+import { logError } from "../utils/logger";
+
+const API_END_POINTS = {
+  searchv1: `${CONSTANTS.CONTENT_API_BASE}/v1/search`,
+};
+
+export const publicContentApi = Router();
+publicContentApi.post("/v1/search", async (req, res) => {
+  try {
+    const body = {
+      ...req.body,
+    };
+    const response = await axios({
+      ...axiosRequestConfig,
+      data: body,
+      headers: {
+        Authorization: CONSTANTS.SB_API_KEY,
+      },
+      method: "POST",
+      url: API_END_POINTS.searchv1,
+    });
+    const contents: IContent[] = response.data.result;
+    if (Array.isArray(contents)) {
+      response.data.result = contents.map((content) => processContent(content));
+    }
+    res.json(
+      response.data || {
+        filters: [],
+        filtersUsed: [],
+        notVisibleFilters: [],
+        result: [],
+        totalHits: 0,
+      }
+    );
+  } catch (err) {
+    logError("SEARCH V6 API ERROR >", err);
+    res.status((err && err.response && err.response.status) || 500).send(
+      (err && err.response && err.response.data) || {
+        error: GENERAL_ERROR_MSG,
+      }
+    );
+  }
+});
