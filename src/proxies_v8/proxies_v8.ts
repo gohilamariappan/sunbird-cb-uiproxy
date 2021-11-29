171475c694972cc0d93b84a90470c8abe8cab9be
Saurav Verma <saurav.verma@tarento.com>
2021-11-29 13:52:50 +0530

    Download certificate proxy created

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -228,6 +228,7 @@ proxiesV8.use('/user/*',
 )
 
 proxiesV8.use('/downloadCertificate/*',
+// tslint:disable-next-line: max-line-length
   proxyCreatorDownloadCertificate(express.Router(), `${CONSTANTS.KONG_API_BASE}/certreg/v2/certs/download/2ad57acb-faf7-40e1-8355-9a46eae51d17`)
 )
 // proxiesV8.use('/api/framework/*',
