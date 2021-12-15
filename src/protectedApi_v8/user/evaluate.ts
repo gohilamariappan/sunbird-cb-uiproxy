9886f9855a7066f5ad50a9a4469b09c672261600
Saurav Verma <saurav.verma@tarento.com>
2021-12-15 14:56:52 +0530

    Submit assessment issue fixes

--- src/protectedApi_v8/user/evaluate.ts
+++ src/protectedApi_v8/user/evaluate.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { axiosRequestConfig } from '../../configs/request.config'
 import { CONSTANTS } from '../../utils/env'
 import { ERROR } from '../../utils/message'
+import { extractUserIdFromRequest, extractUserToken } from '../../utils/requestExtract'
 
 const GENERAL_ERR_MSG = 'Failed due to unknown reason'
 const API_END_POINTS = {
@@ -11,6 +12,42 @@ const API_END_POINTS = {
   postAssessment: `${CONSTANTS.POST_ASSESSMENT_BASE}/lmsapi/v1/post_assessment`,
 }
 export const evaluateApi = Router()
+
+evaluateApi.post('/assessment/submit/v2', async (req, res) => {
+  try {
+    const org = req.header('org')
+    const rootOrg = req.header('rootOrg')
+    if (!org || !rootOrg) {
+      res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
+      return
+    }
+    const userId = extractUserIdFromRequest(req)
+    const url = `${API_END_POINTS.assessmentSubmitV2}/assessment/submit`
+    const requestBody = {
+      ...req.body,
+    }
+    const response = await axios({
+      ...axiosRequestConfig,
+      data: requestBody,
+      headers: {
+        Authorization: CONSTANTS.SB_API_KEY,
+        rootOrg,
+        userId,
+        'x-authenticated-user-token': extractUserToken(req),
+      },
+      method: 'POST',
+      url,
+    })
+    res.status(response.status).send(response.data)
+  } catch (err) {
+    res.status((err && err.response && err.response.status) || 500).send(
+      (err && err.response && err.response.data) || {
+        error: GENERAL_ERR_MSG,
+      }
+    )
+  }
+})
+
 evaluateApi.post('/assessment/submit/iap', async (req, res) => {
   const url = `${API_END_POINTS.iapSubmitAssessment}`
   const requestBody = {
@@ -30,13 +67,10 @@ evaluateApi.post('/assessment/submit/iap', async (req, res) => {
       res.status(response.status).send(response.data)
     })
     .catch((error) => {
-      res
-        .status((error && error.response && error.response.status) || 500)
-        .send(
-          (error && error.response && error.response.data) || {
-            error: GENERAL_ERR_MSG,
-          }
-        )
+      res.status((error && error.response && error.response.status) || 500)
+        .send((error && error.response && error.response.data) || {
+          error: GENERAL_ERR_MSG,
+        })
     })
 })
 
@@ -63,10 +97,10 @@ evaluateApi.get('/post-assessment/:contentId', async (req, res) => {
     })
     res.send(response.data)
   } catch (err) {
-    res.status((err && err.response && err.response.status) || 500).send(
-      (err && err.response && err.response.data) || {
+    res
+      .status((err && err.response && err.response.status) || 500)
+      .send((err && err.response && err.response.data) || {
         error: GENERAL_ERR_MSG,
-      }
-    )
+      })
   }
 })
