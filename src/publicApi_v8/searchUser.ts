6f779758bc89f4ebfbdaafb98ebc3499ba042c7c
Saurav Verma <saurav.verma@tarento.com>
2021-12-22 11:52:49 +0530

    Search user split fix

--- src/publicApi_v8/searchUser.ts
+++ src/publicApi_v8/searchUser.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import _ from 'lodash'
 import { axiosRequestConfig } from '../configs/request.config'
 import { CONSTANTS } from '../utils/env'
-import { logInfo } from '../utils/logger'
+import { logInfo, logError } from '../utils/logger'
 
 const API_END_POINTS = {
   searchSb: `${CONSTANTS.LEARNER_SERVICE_API_BASE}/private/user/v1/search`,
@@ -11,6 +11,7 @@ const API_END_POINTS = {
 export const fetchUser = async (searchValue: string, searchType: string) => {
   logInfo('Search User - Entered in phone and value is : ', searchValue)
   logInfo('Search Type - Entered in phone and type is : ', searchType)
+  try {
   const userSearchResponse = await axios({
     ...axiosRequestConfig,
     data: {
@@ -27,4 +28,7 @@ export const fetchUser = async (searchValue: string, searchType: string) => {
   })
   logInfo('Fetch User Response : ' + JSON.stringify(userSearchResponse))
   return userSearchResponse
+} catch (err) {
+    logError('ERROR FETCHING Search User '+err)
+}
 }
