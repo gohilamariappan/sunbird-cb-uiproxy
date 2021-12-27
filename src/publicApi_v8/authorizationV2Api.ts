212300c73a2068d95e6b8f91b7ae2400e7c9393d
Saurav Verma <saurav.verma@tarento.com>
2021-12-27 15:13:05 +0530

    Cookie Generation

--- src/publicApi_v8/authorizationV2Api.ts
+++ src/publicApi_v8/authorizationV2Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import _ from 'lodash'
-import qs from 'querystring'
+//import qs from 'querystring'
 import { axiosRequestConfig } from '../configs/request.config'
 import { setSessionConfig } from '../configs/session.config'
 import { CONSTANTS } from '../utils/env'
@@ -11,32 +11,36 @@ const API_END_POINTS = {
   verfifyToken  : `https://aastrika-sb.idc.tarento.com/auth/realms/sunbird/protocol/openid-connect/userinfo`,
 }
 export const authorizationV2Api = async (username: string, password: string) => {
-  logInfo('Entered into authorization part.')
+    logInfo('Entered into authorization part.')
 
-  const encodedData = qs.stringify({
-                                    client_id: 'portal',
-                                    client_secret: `${CONSTANTS.KEYCLOAK_CLIENT_SECRET}`,
-                                    grant_type: 'password',
-                                    password,
-                                    username,
-                                    })
-  logInfo('Entered into authorization part.', encodedData)
-  const authTokenResponse = await axios({
-        ...axiosRequestConfig,
-        data: encodedData,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        method: 'POST',
-        url: API_END_POINTS.generateToken,
-    })
+    const encodedData = {
+                            client_id: 'portal',
+                            client_secret: `${CONSTANTS.KEYCLOAK_CLIENT_SECRET}`,
+                            grant_type: 'password',
+                            password,
+                            username,
+                        }
+    logInfo('Entered into authorization part.'+ encodedData)
+    const authTokenResponse = await axios({
+            ...axiosRequestConfig,
+            data: encodedData,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            method: 'POST',
+            url: API_END_POINTS.generateToken,
+        })
 
-  const accessToken = authTokenResponse.data.access_token
+    logInfo('authTokenResponse :' + JSON.stringify(authTokenResponse))
 
-  logInfo('authTokenResponse :' + JSON.stringify(authTokenResponse))
-  logInfo('accessToken ' + accessToken)
+    const accessToken = authTokenResponse.data.access_token
 
-  if (accessToken) {
+    logInfo('accessToken ' + accessToken)
+
+    if (accessToken) {
+        
+        logInfo('Entered into accessToken : ')
+
         const userTokenResponse = await axios({
             ...axiosRequestConfig,
             headers: {
@@ -46,10 +50,12 @@ export const authorizationV2Api = async (username: string, password: string) => 
 
             url: API_END_POINTS.verfifyToken,
         })
+        
         logInfo('userTokenResponse : ', JSON.stringify(userTokenResponse))
+
         if (userTokenResponse.data.name) {
             logInfo('Success ! Entered into setting cookie')
-            setSessionConfig()
+            setSessionConfig()  
         }
     }
   return true
