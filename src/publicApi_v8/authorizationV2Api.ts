783541e20fd7234bc4520c9ae626d1ed924fd144
amantarento <amankumar.sharma@tarento.com>
2021-12-31 18:27:03 +0530

    delete the set session test

--- src/publicApi_v8/authorizationV2Api.ts
+++ src/publicApi_v8/authorizationV2Api.ts
@@ -1,72 +1,59 @@
-import axios from 'axios'
-import expressSession from 'express-session'
-import _ from 'lodash'
-import qs from 'querystring'
-import { axiosRequestConfig } from '../configs/request.config'
-import { getSessionConfig } from '../configs/session.config'
-import { CONSTANTS } from '../utils/env'
-import { logInfo } from '../utils/logger'
+import axios from "axios";
+import _ from "lodash";
+import qs from "querystring";
+import { axiosRequestConfig } from "../configs/request.config";
+import { CONSTANTS } from "../utils/env";
+import { logInfo } from "../utils/logger";
 const API_END_POINTS = {
   generateToken: `https://aastrika-sb.idc.tarento.com/auth/realms/sunbird/protocol/openid-connect/token`,
   verfifyToken: `https://aastrika-sb.idc.tarento.com/auth/realms/sunbird/protocol/openid-connect/userinfo`,
-}
+};
 export const authorizationV2Api = async (
   username: string,
-  password: string,
-  // tslint:disable-next-line: no-any
-  req: any
+  password: string
 ) => {
-  logInfo('Entered into authorizationV2Api ')
+  logInfo("Entered into authorizationV2Api ");
 
   const encodedData = qs.stringify({
-    client_id: 'portal',
+    client_id: "portal",
     client_secret: `${CONSTANTS.KEYCLOAK_CLIENT_SECRET}`,
-    grant_type: 'password',
+    grant_type: "password",
     password,
     username,
-  })
-  logInfo('Entered into authorization part.' + encodedData)
+  });
+  logInfo("Entered into authorization part." + encodedData);
 
   try {
     const authTokenResponse = await axios({
       ...axiosRequestConfig,
       data: encodedData,
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      method: 'POST',
+      method: "POST",
       url: API_END_POINTS.generateToken,
-    })
+    });
 
-    logInfo('Entered into authTokenResponse :' + authTokenResponse)
+    logInfo("Entered into authTokenResponse :" + authTokenResponse);
 
-    const accessToken = authTokenResponse.data.access_token
+    const accessToken = authTokenResponse.data.access_token;
     if (accessToken) {
       const userTokenResponse = await axios({
         ...axiosRequestConfig,
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
-        method: 'GET',
+        method: "GET",
 
         url: API_END_POINTS.verfifyToken,
-      })
-      logInfo('userTokenResponse : ' + userTokenResponse)
+      });
+      logInfo("userTokenResponse : " + userTokenResponse);
       if (userTokenResponse.data.name) {
-        logInfo('Success ! Entered into usertokenResponse..')
-        const sessionConfiguration = getSessionConfig()
-        logInfo('Entered into set session Config.. ')
-        req.app.use(expressSession(sessionConfiguration))
-        logInfo('req.session ' + req.session)
-        if (_.isEmpty(req.session)) {
-          logInfo('req.session is empty ' + req.session)
-        } else {
-          logInfo('req.session is  not empty ' + req.session)
-        }
+        logInfo("Success ! Entered into usertokenResponse..");
       }
     }
   } catch (e) {
-    logInfo('Error throwing Cookie : ' + e)
+    logInfo("Error throwing Cookie : " + e);
   }
-  return true
-}
+  return true;
+};
