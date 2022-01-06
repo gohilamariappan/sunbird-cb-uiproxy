c3d408e08ee0cda2ae73ff3b025a626c7f727eee
Saurav Verma <saurav.verma@tarento.com>
2022-01-06 10:54:42 +0530

    Change in session config

--- src/publicApi_v8/rolePermission.ts
+++ src/publicApi_v8/rolePermission.ts
@@ -1,80 +1,80 @@
-import axios from "axios";
-import _ from "lodash";
-import { axiosRequestConfig } from "../configs/request.config";
-import { CONSTANTS } from "../utils/env";
-import { logError, logInfo } from "../utils/logger";
+import axios from 'axios'
+import _ from 'lodash'
+import { axiosRequestConfig } from '../configs/request.config'
+import { CONSTANTS } from '../utils/env'
+import { logError, logInfo } from '../utils/logger'
 const ROLE = [
-  "CBC_ADMIN",
-  "CBC_MEMBER",
-  "CONTENT_CREATOR",
-  "CONTENT_PUBLISHER",
-  "CONTENT_REVIEWER",
-  "EDITOR",
-  "FRAC_COMPETENCY_MEMBER",
-  "FRAC_ADMIN",
-  "FRAC_COMPETENCY_REVIEWER",
-  "FRAC_REVIEWER_L1",
-  "FRAC_REVIEWER_L2",
-  "IFU_MEMBER",
-  "MDO_ADMIN",
-  "PUBLIC",
-  "SPV_ADMIN",
-  "WAT_MEMBER",
-];
+  'CBC_ADMIN',
+  'CBC_MEMBER',
+  'CONTENT_CREATOR',
+  'CONTENT_PUBLISHER',
+  'CONTENT_REVIEWER',
+  'EDITOR',
+  'FRAC_COMPETENCY_MEMBER',
+  'FRAC_ADMIN',
+  'FRAC_COMPETENCY_REVIEWER',
+  'FRAC_REVIEWER_L1',
+  'FRAC_REVIEWER_L2',
+  'IFU_MEMBER',
+  'MDO_ADMIN',
+  'PUBLIC',
+  'SPV_ADMIN',
+  'WAT_MEMBER',
+]
 
 // tslint:disable-next-line: no-any
 export const setRolesData = (reqObj: any, body: any) => {
   // tslint:disable-next-line: no-any
-  const userData: any = body;
+  const userData: any = body
 
-  logInfo("userData" + JSON.stringify(userData));
+  logInfo('userData' + JSON.stringify(userData))
   if (reqObj.session) {
     reqObj.session.userId = userData.result.response.id
       ? userData.result.response.id
-      : userData.result.response.userId;
-    reqObj.session.userName = userData.result.response.userName;
+      : userData.result.response.userId
+    reqObj.session.userName = userData.result.response.userName
     // reqObj.session.userRoles = userData.result.response.roles
-    reqObj.session.userRoles = ROLE ? ROLE : [];
-    reqObj.session.orgs = userData.result.response.organisations;
-    reqObj.session.rootOrgId = userData.result.response.rootOrgId;
+    reqObj.session.userRoles = ROLE ? ROLE : []
+    reqObj.session.orgs = userData.result.response.organisations
+    reqObj.session.rootOrgId = userData.result.response.rootOrgId
     // userData.roles.push(ROLE)
-    if (!_.includes(reqObj.session.userRoles, "PUBLIC")) {
-      reqObj.session.userRoles.push("PUBLIC");
+    if (!_.includes(reqObj.session.userRoles, 'PUBLIC')) {
+      reqObj.session.userRoles.push('PUBLIC')
     }
-    logInfo("reqObj.session >>>>>>>", JSON.stringify(reqObj.session));
+    logInfo('reqObj.session >>>>>>>', JSON.stringify(reqObj.session))
     // tslint:disable-next-line: no-any
     reqObj.session.save((error: any) => {
       if (error) {
-        logError("Error while saving the roles");
+        logError('Error while saving the roles')
       } else {
-        logInfo("Session saved successfully");
+        logInfo('Session saved successfully')
       }
-    });
+    })
   }
-};
+}
 // tslint:disable-next-line: no-any
 export const getCurrentUserRoles = async (reqObj: any, accessToken: any) => {
   // console.log('Step 3: Get user roles function')
-  logInfo("calling getCurrentUserRoles >>>>");
-  const userId = reqObj.session.userId;
-  logInfo("get userId from session >>>>" + userId);
+  logInfo('calling getCurrentUserRoles >>>>')
+  const userId = reqObj.session.userId
+  logInfo('get userId from session >>>>' + userId)
   // console.log(userId)
-  const readUrl = `${CONSTANTS.SUNBIRD_PROXY_API_BASE}/user/v2/read/` + userId;
+  const readUrl = `${CONSTANTS.SUNBIRD_PROXY_API_BASE}/user/v2/read/` + userId
   const authTokenResponse = await axios({
     ...axiosRequestConfig,
 
     headers: {
       Authorization: CONSTANTS.SB_API_KEY,
-      "X-Channel-Id": CONSTANTS.X_Channel_Id,
-      "x-authenticated-user-token": accessToken,
-      "x-authenticated-userid": userId,
+      'X-Channel-Id': CONSTANTS.X_Channel_Id,
+      'x-authenticated-user-token': accessToken,
+      'x-authenticated-userid': userId,
     },
-    method: "GET",
+    method: 'GET',
     url: readUrl,
-  });
+  })
   // tslint:disable-next-line: no-any
-  logInfo("getAuthTokenResponse :" + authTokenResponse);
+  logInfo('getAuthTokenResponse :' + authTokenResponse)
   if (authTokenResponse) {
-    setRolesData(reqObj, authTokenResponse.data);
+    setRolesData(reqObj, authTokenResponse.data)
   }
-};
+}
--- src/server.ts
+++ src/server.ts
@@ -1,126 +1,96 @@
-import compression from "compression";
-import connectTimeout from "connect-timeout";
-import cors from "cors";
-import express, { NextFunction } from "express";
-import fileUpload from "express-fileupload";
-import expressSession from "express-session";
-import helmet from "helmet";
-import morgan from "morgan";
-import { authContent } from "./authoring/authContent";
-import { authIapBackend } from "./authoring/authIapBackend";
-import { authNotification } from "./authoring/authNotification";
-import { authSearch } from "./authoring/authSearch";
-import { authApi } from "./authoring/content";
-import { getSessionConfig } from "./configs/session.config";
-import { protectedApiV8 } from "./protectedApi_v8/protectedApiV8";
-import { proxiesV8 } from "./proxies_v8/proxies_v8";
-import { publicApiV8 } from "./publicApi_v8/publicApiV8";
-import { CustomKeycloak } from "./utils/custom-keycloak";
-import { CONSTANTS } from "./utils/env";
-import { logInfo, logSuccess } from "./utils/logger";
-const cookieParser = require("cookie-parser");
-const healthcheck = require("express-healthcheck");
-import { apiWhiteListLogger, isAllowed } from "./utils/apiWhiteList";
+import compression from 'compression'
+import connectTimeout from 'connect-timeout'
+import cors from 'cors'
+import express, { NextFunction } from 'express'
+import fileUpload from 'express-fileupload'
+import expressSession from 'express-session'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { authContent } from './authoring/authContent'
+import { authIapBackend } from './authoring/authIapBackend'
+import { authNotification } from './authoring/authNotification'
+import { authSearch } from './authoring/authSearch'
+import { authApi } from './authoring/content'
+import { getSessionConfig } from './configs/session.config'
+import { protectedApiV8 } from './protectedApi_v8/protectedApiV8'
+import { proxiesV8 } from './proxies_v8/proxies_v8'
+import { publicApiV8 } from './publicApi_v8/publicApiV8'
+import { CustomKeycloak } from './utils/custom-keycloak'
+import { CONSTANTS } from './utils/env'
+import { logInfo, logSuccess } from './utils/logger'
+const cookieParser = require('cookie-parser')
+const healthcheck = require('express-healthcheck')
 
-function haltOnTimedOut(
-  req: Express.Request,
-  _: Express.Response,
-  next: NextFunction
-) {
+import { apiWhiteListLogger, isAllowed } from './utils/apiWhiteList'
+
+function haltOnTimedOut(req: Express.Request, _: Express.Response, next: NextFunction) {
   if (!req.timedout) {
-    next();
+    next()
   }
 }
 export class Server {
-  // tslint:disable-next-line: no-any
-  static app: any;
   static bootstrap() {
-    const server = new Server();
-    server.app.listen(CONSTANTS.PORTAL_PORT, "0.0.0.0", () => {
-      logSuccess(`${process.pid} : Server started at ${CONSTANTS.PORTAL_PORT}`);
-    });
+    const server = new Server()
+    server.app.listen(CONSTANTS.PORTAL_PORT, '0.0.0.0', () => {
+      logSuccess(`${process.pid} : Server started at ${CONSTANTS.PORTAL_PORT}`)
+    })
   }
 
-  protected app = express();
-  private keycloak?: CustomKeycloak;
+  protected app = express()
+  private keycloak?: CustomKeycloak
   private constructor() {
-    if (CONSTANTS.CORS_ENVIRONMENT === "dev") {
-      this.app.use(
-        cors({ origin: "https://local.igot-dev.in:3000", credentials: true })
-      );
+    if (CONSTANTS.CORS_ENVIRONMENT === 'dev') {
+      this.app.use(cors({origin: 'https://local.igot-dev.in:3000', credentials: true}))
     } else {
-      this.app.use(cors());
+      this.app.use(cors())
     }
     const sessionConfig = getSessionConfig()
     this.app.use(expressSession(sessionConfig))
-    // tslint:disable-next-line: no-any
-    this.app.all("*", apiWhiteListLogger());
-    if (CONSTANTS.PORTAL_API_WHITELIST_CHECK === "true") {
-      logInfo("Failed ! Entered inside API whitelist check..");
-      this.app.all("*", isAllowed());
+    this.app.all('*', apiWhiteListLogger())
+    if (CONSTANTS.PORTAL_API_WHITELIST_CHECK === 'true') {
+      this.app.all('*', isAllowed())
     }
-    this.setCookie();
-    this.setKeyCloak(sessionConfig);
-    this.authoringProxies();
-    this.configureMiddleware();
-    this.servePublicApi();
-    this.serverProtectedApi();
-    this.serverProxies();
-    this.authoringApi();
-    this.resetCookies();
-    this.app.use(haltOnTimedOut);
+    this.setCookie()
+    this.setKeyCloak(sessionConfig)
+    this.authoringProxies()
+    this.configureMiddleware()
+    this.servePublicApi()
+    this.serverProtectedApi()
+    this.serverProxies()
+    this.authoringApi()
+    this.resetCookies()
+    this.app.use(haltOnTimedOut)
   }
+
   private setCookie() {
-    this.app.use(cookieParser());
-    this.app.use(
-      (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        const rootOrg = req.headers
-          ? req.headers.rootOrg || req.headers.rootorg
-          : "";
-        if (rootOrg && req.hostname.toLowerCase().includes("localhost")) {
-          logInfo("Entered into SetCookie 1st part..." + rootOrg);
-          res.cookie("rootorg", rootOrg);
-        }
-        next();
+    this.app.use(cookieParser())
+    this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+      const rootOrg = req.headers ? req.headers.rootOrg || req.headers.rootorg : ''
+      if (rootOrg && req.hostname.toLowerCase().includes('localhost')) {
+        res.cookie('rootorg', rootOrg)
       }
-    );
-    this.app.use(
-      (
-        _req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        logInfo("Entered into SetCookie 2nd part...");
-        res.header(
-          "Cache-Control",
-          "private, no-cache, no-store, must-revalidate"
-        );
-        res.header("Expires", "-1");
-        res.header("Pragma", "no-cache");
-        next();
-      }
-    );
+      next()
+    })
+    this.app.use((_req: express.Request, res: express.Response, next: express.NextFunction) => {
+      res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+      res.header('Expires', '-1')
+      res.header('Pragma', 'no-cache')
+      next()
+    })
   }
 
   private configureMiddleware() {
-    this.app.use(connectTimeout("240s"));
-    this.app.use(compression());
-    this.app.use(express.urlencoded({ extended: false, limit: "50mb" }));
-    this.app.use(express.json({ limit: "50mb" }));
-    this.app.use(fileUpload());
+    this.app.use(connectTimeout('240s'))
+    this.app.use(compression())
+    this.app.use(express.urlencoded({ extended: false, limit: '50mb' }))
+    this.app.use(express.json({ limit: '50mb' }))
+    this.app.use(fileUpload())
     // this.app.use(cors())
-    this.app.use(
-      "/healthcheck",
-      healthcheck({
-        healthy() {
-          return { everything: "is ok" };
-        },
-      })
-    );
+    this.app.use('/healthcheck', healthcheck({
+      healthy() {
+        return { everything: 'is ok' }
+      },
+    }))
     this.app.use(
       helmet({
         contentSecurityPolicy: {
@@ -129,19 +99,19 @@ export class Server {
           },
         },
         dnsPrefetchControl: { allow: true },
-        frameguard: { action: "sameorigin" },
+        frameguard: { action: 'sameorigin' },
         hidePoweredBy: true,
         ieNoOpen: true,
         noCache: false,
         noSniff: true,
       })
-    );
+    )
     // TODO: See what needs to be logged
     this.app.use((req, _, next) => {
-      logInfo(`Worker ${process.pid} : ${req.url}`);
-      next();
-    });
-    this.app.use(morgan("dev"));
+      logInfo(`Worker ${process.pid} : ${req.url}`)
+      next()
+    })
+    this.app.use(morgan('dev'))
     this.app.use(
       morgan((tokens: morgan.TokenIndexer, req, res) =>
         [
@@ -149,59 +119,54 @@ export class Server {
           tokens.method(req, res),
           tokens.url(req, res),
           tokens.status(req, res),
-          tokens.res(req, res, "content-length"),
-          "-",
-          tokens["response-time"](req, res),
-          "ms",
+          tokens.res(req, res, 'content-length'),
+          '-',
+          tokens['response-time'](req, res),
+          'ms',
           `timeout: ${CONSTANTS.TIMEOUT}`,
-        ].join(" ")
+        ].join(' ')
       )
-    );
-    this.app.use(haltOnTimedOut);
+    )
+    this.app.use(haltOnTimedOut)
   }
   // tslint:disable-next-line: no-any
   private setKeyCloak(sessionConfig: any) {
-    this.keycloak = new CustomKeycloak(sessionConfig);
-    logInfo("Entered into Setkeycloak...");
-    this.app.use(this.keycloak.middleware);
+    this.keycloak = new CustomKeycloak(sessionConfig)
+    this.app.use(this.keycloak.middleware)
   }
 
   private servePublicApi() {
-    this.app.use("/public/v8", publicApiV8);
+    this.app.use('/public/v8', publicApiV8)
   }
 
   private serverProtectedApi() {
     if (this.keycloak) {
-      this.app.use("/protected/v8", this.keycloak.protect, protectedApiV8);
+      this.app.use('/protected/v8', this.keycloak.protect, protectedApiV8)
     }
   }
   private serverProxies() {
     if (this.keycloak) {
-      this.app.use("/proxies/v8", this.keycloak.protect, proxiesV8);
+      this.app.use('/proxies/v8', this.keycloak.protect, proxiesV8)
     }
   }
   private authoringProxies() {
     if (this.keycloak) {
-      this.app.use("/authContent", this.keycloak.protect, authContent);
-      this.app.use(
-        "/authNotificationApi",
-        this.keycloak.protect,
-        authNotification
-      );
-      this.app.use("/authIapApi", this.keycloak.protect, authIapBackend);
+      this.app.use('/authContent', this.keycloak.protect, authContent)
+      this.app.use('/authNotificationApi', this.keycloak.protect, authNotification)
+      this.app.use('/authIapApi', this.keycloak.protect, authIapBackend)
     }
   }
   private authoringApi() {
     if (this.keycloak) {
-      this.app.use("/authSearchApi", this.keycloak.protect, authSearch);
-      this.app.use("/authApi", authApi);
+      this.app.use('/authSearchApi', this.keycloak.protect, authSearch)
+      this.app.use('/authApi', authApi)
     }
   }
   private resetCookies() {
-    this.app.use("/reset", (_req, res) => {
-      logInfo("==========================\nCLEARING RES COOKIES");
-      res.clearCookie("connect.sid");
-      res.status(200).send();
-    });
+    this.app.use('/reset', (_req, res) => {
+      logInfo('==========================\nCLEARING RES COOKIES')
+      res.clearCookie('connect.sid')
+      res.status(200).send()
+    })
   }
 }
