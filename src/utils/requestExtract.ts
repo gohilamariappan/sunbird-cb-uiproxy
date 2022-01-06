2931fcc69dfbdb482f54a2491e3468d6c04d627d
amantarento <amankumar.sharma@tarento.com>
2022-01-06 14:16:44 +0530

    change in keyklock config

--- src/utils/requestExtract.ts
+++ src/utils/requestExtract.ts
@@ -1,10 +1,10 @@
-import { Request } from 'express'
-import uuid from 'uuid'
+import { Request } from "express";
+import uuid from "uuid";
 export interface IAuthorizedRequest extends Request {
   kauth?: {
     grant: {
       access_token: {
-        token: string
+        token: string;
         content: {
           given_name: string;
           family_name: string;
@@ -16,61 +16,65 @@ export interface IAuthorizedRequest extends Request {
         };
       };
     };
-  }
+  };
 }
-export const extractUserIdFromRequest = (req: IAuthorizedRequest): string => {
-  const wid = req.header('wid')
+// tslint:disable-next-line: no-any
+export const extractUserIdFromRequest = (req: any): string => {
+  const wid = req.header("wid");
   if (wid) {
-    return wid
+    return wid;
   }
-  return (req.kauth && req.kauth.grant.access_token.content.sub) as string
-}
+  return req.session.userId as string;
+};
 
 export const extractUserId = (req: IAuthorizedRequest): string => {
-  const wid = req.header('wid')
+  const wid = req.header("wid");
   if (wid) {
-    return wid
+    return wid;
   }
-  const userId = (req.kauth && req.kauth.grant.access_token.content.sub) as string
-  return userId.split(':')[2]
-}
+  const userId = (req.kauth &&
+    req.kauth.grant.access_token.content.sub) as string;
+  return userId.split(":")[2];
+};
 
 export const extractUserNameFromRequest = (req: IAuthorizedRequest) =>
-  (req.kauth && req.kauth.grant.access_token.content.name) as string
+  (req.kauth && req.kauth.grant.access_token.content.name) as string;
 
 export const extractUserEmailFromRequest = (req: IAuthorizedRequest) =>
   ((req.kauth && req.kauth.grant.access_token.content.email) ||
     (req.kauth &&
-      req.kauth.grant.access_token.content.preferred_username)) as string
+      req.kauth.grant.access_token.content.preferred_username)) as string;
 
 export const extractUserSessionState = (req: IAuthorizedRequest) =>
-  (req.kauth && req.kauth.grant.access_token.content.session_state) as string
+  (req.kauth && req.kauth.grant.access_token.content.session_state) as string;
 
 export const extractUserTokenContent = (req: IAuthorizedRequest) => {
-  return req.kauth && req.kauth.grant.access_token.content
-}
+  return req.kauth && req.kauth.grant.access_token.content;
+};
 
 export const extractUserToken = (req: IAuthorizedRequest) => {
-  return req.kauth && req.kauth.grant.access_token.token
-}
+  return req.kauth && req.kauth.grant.access_token.token;
+};
 
-export const extractAuthorizationFromRequest = (req: IAuthorizedRequest): string => {
-  const token = req.kauth && req.kauth.grant.access_token.token
+export const extractAuthorizationFromRequest = (
+  req: IAuthorizedRequest
+): string => {
+  const token = req.kauth && req.kauth.grant.access_token.token;
   // Bearer is added as other areas are using split function to get the token
-  return 'Bearer ' + token
-}
-export const extractUserTokenFromRequest = (req: IAuthorizedRequest): string => {
-  const xAuthorization = req.header('X-Authenticated-User-Token')
+  return "Bearer " + token;
+};
+export const extractUserTokenFromRequest = (
+  req: IAuthorizedRequest
+): string => {
+  const xAuthorization = req.header("X-Authenticated-User-Token");
 
-  return xAuthorization as string
-
-}
+  return xAuthorization as string;
+};
 
 export const extractRootOrgFromRequest = (req: IAuthorizedRequest): string => {
-  const rootOrg = req.header('rootorg')
+  const rootOrg = req.header("rootorg");
 
-  return rootOrg as string
+  return rootOrg as string;
+};
 
-}
-
-export const getUUID = () => uuid.v1()
+export const getUUID = () => uuid.v1();
