64cc8c61c7759f7c36f23b6e8a96201bbe43c9bb
Saurav Verma <saurav.verma@tarento.com>
2022-01-07 10:56:25 +0530

    change in validate api

--- src/configs/keycloak.config.ts
+++ src/configs/keycloak.config.ts
@@ -9,5 +9,6 @@ export function getKeycloakConfig(url?: string, realm?: string) {
     'public-client': true,
     realm: realm ? realm : CONSTANTS.KEYCLOAK_REALM,
     resource: 'portal',
+    'auth-server-url': url ? `${url}` : `${CONSTANTS.HTTPS_HOST}/auth`,
   }
 }
