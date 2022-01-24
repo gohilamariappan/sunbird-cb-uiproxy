fdb63352632dda5bcc9ed67157c5489228ba1b5d
Saurav Verma <saurav.verma@tarento.com>
2022-01-24 19:24:32 +0530

    Whitelist issue fix

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -238,6 +238,11 @@ export const API_LIST = {
       // tslint:disable-next-line: object-literal-sort-keys
       ROLE_CHECK: [ROLE.PUBLIC],
     },
+    '/proxies/v8/getContents/content/html/:do_id': {
+      checksNeeded: [CHECK.ROLE],
+      // tslint:disable-next-line: object-literal-sort-keys
+      ROLE_CHECK: [ROLE.PUBLIC],
+    },
     '/proxies/v8/upload/action/content/v3/upload/:do_id': {
       checksNeeded: [CHECK.ROLE],
       // tslint:disable-next-line: object-literal-sort-keys
@@ -1115,6 +1120,7 @@ export const API_LIST = {
     '/proxies/v8/getContents',
     '/proxies/v8/logout/user',
     '/proxies/v8/getContents/content/:do_id/artifact/:do_id',
+    '/proxies/v8/getContents/content/html/:do_id',
     '/proxies/v8/v1/content/retire',
     '/proxies/v8/action/content/v3/updateReviewStatus/:do_id',
     '/proxies/v8/action/content/v3/review/:do_id',
