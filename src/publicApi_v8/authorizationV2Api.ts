c7926b6cd71803b3e87764e0131a4471e655d648
Saurav Verma <saurav.verma@tarento.com>
2022-02-10 10:10:22 +0530

    config changes

--- src/publicApi_v8/authorizationV2Api.ts
+++ src/publicApi_v8/authorizationV2Api.ts
@@ -19,9 +19,9 @@ export const authorizationV2Api = async (
 ) => {
 
   const encodedData = qs.stringify({
-    client_id: 'lms',
-    client_secret: `${CONSTANTS.KEYCLOAK_CLIENT_SECRET}`,
-    grant_type: 'client_credentials',
+    client_id: 'portal',
+   // client_secret: `${CONSTANTS.KEYCLOAK_CLIENT_SECRET}`,
+    grant_type: 'password',
     password,
     username,
   })
