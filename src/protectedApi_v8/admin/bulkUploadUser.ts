063dbca4a1ae8abb2fc1895a2098ffa8d0f6ba94
Saurav Verma <saurav.verma@tarento.com>
2022-07-10 23:27:25 +0530

    Bulk Upload Updated

--- src/protectedApi_v8/admin/bulkUploadUser.ts
+++ src/protectedApi_v8/admin/bulkUploadUser.ts
@@ -14,62 +14,62 @@ export const bulkUploadUserApi = Router()
 
 bulkUploadUserApi.post('/create-users', async (req: any, res) => {
 
-	try {
-		const fileData = req.files.userData.data.toString('utf8')
-		logInfo('files recived >>>>>>>>> ' + fileData)
-		const lines = fileData.split('\n')
-	 logInfo('Linesdata >>>>>' + lines)
-		const result = []
+    try {
+        const fileData = req.files.userData.data.toString('utf8')
+        logInfo('files recived >>>>>>>>> ' + fileData)
+        const lines = fileData.split('\n')
+     logInfo('Linesdata >>>>>' + lines)
+        const result = []
 
-		// NOTE: If your columns contain commas in their values, you'll need
-		const headers = lines[0].split(',')
-		logInfo('lines.length >>>>>>' + lines.length)
-		for (let i = 1; i < lines.length; i++) {
+        // NOTE: If your columns contain commas in their values, you'll need
+        const headers = lines[0].split(',')
+        logInfo('lines.length >>>>>>' + lines.length)
+        for (let i = 1; i < lines.length; i++) {
 
-			const obj = {}
-			const currentline = lines[i].split(',')
-	  for (let j = 0; j < headers.length; j++) {
-				obj[headers[j]] = currentline[j]
-			}
+            const obj = {}
+            const currentline = lines[i].split(',')
+      for (let j = 0; j < headers.length; j++) {
+                obj[headers[j]] = currentline[j]
+            }
 
-			result.push(obj)
-		}
+            result.push(obj)
+        }
 
-		const finalResult = result.map(async (csvObjData: any) => {
+        const finalResult = result.map(async (csvObjData: any) => {
 
-			logInfo('Entered into CSV object data >>>>>', JSON.stringify(csvObjData))
+            logInfo('Entered into CSV object data >>>>>', JSON.stringify(csvObjData))
 
-			const collectData = {
-				firstName: csvObjData.first_name,
-				lastName: csvObjData.last_name,
-				// username: csvObjData.username,
-				[csvObjData.type]: csvObjData.phone ? csvObjData.phone : csvObjData.username,
-				password: process.env.PASSWORD,
-				roles : csvObjData.roles,
-				usertype : csvObjData.usertype,
-			}
+            const collectData = {
+                firstName: csvObjData.first_name,
+                lastName: csvObjData.last_name,
+                // username: csvObjData.username,
+                [csvObjData.type]: csvObjData.phone ? csvObjData.phone : csvObjData.username,
+                password: process.env.PASSWORD,
+                roles : csvObjData.roles,
+                usertype : csvObjData.usertype,
+            }
 
-			logInfo('collectData >>>>>' + JSON.stringify(collectData))
+            logInfo('collectData >>>>>' + JSON.stringify(collectData))
 
-			const responseUserCreation = await axios({
-				...axiosRequestConfig,
-				data: {  request: collectData },
-				headers: {
-				  Authorization: CONSTANTS.SB_API_KEY,
-				},
-				method: 'POST',
-				url: API_ENDPOINTS.createUserOfBulkUpload,
-			  })
-			logInfo('Check user creation >> ' + responseUserCreation)
+            const responseUserCreation = await axios({
+                ...axiosRequestConfig,
+                data: {  request: collectData },
+                headers: {
+                  Authorization: CONSTANTS.SB_API_KEY,
+                },
+                method: 'POST',
+                url: API_ENDPOINTS.createUserOfBulkUpload,
+              })
+            logInfo('Check user creation >> ' + responseUserCreation)
 
-		})
-		logInfo("Final result output for bulk upload : "+finalResult)
-	} catch (error) {
-			logInfo('catch error-' + error)
-			res.status(500).send({
-					message: 'Could not upload the file: ',
-			})
-	}
-	return res.status(200).send('Success')
+        })
+        logInfo('Final result output for bulk upload : ' + finalResult)
+    } catch (error) {
+            logInfo('catch error-' + error)
+            res.status(500).send({
+                    message: 'Could not upload the file: ',
+            })
+    }
+    return res.status(200).send('Success')
 
 })
