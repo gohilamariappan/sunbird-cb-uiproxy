30bccc19efc35e04ee68339973cdc751099e1193
Saurav Verma <saurav.verma@tarento.com>
2022-07-27 08:59:26 +0530

    Discussion Category List api : Topics Sorting issue fixed

--- src/proxies_v8/proxies_v8.ts
+++ src/proxies_v8/proxies_v8.ts
@@ -355,15 +355,14 @@ proxiesV8.use('/discussion/category/list', (req: any, res) => {
               method: 'post',
               url: CONSTANTS.SUNBIRD_PROXY_API_BASE + '/discussion/category/list?_uid=' + req.session.nodebbUid,
           })
-    // tslint:disable-next-line: no-any
-    // tslint:disable-next-line: only-arrow-functions,  no-any
+    // tslint:disable-next-line: all
     .then(function(response: any)  {
       // tslint:disable-next-line: no-any
       response.data.result[0].topics.sort((a: any, b: any) => b.mainPid - a.mainPid)
       // logInfo("Filtered Data >>>>>>>>>>>>>>>>>>>>"+JSON.stringify(response.data))
       res.send({message: 'Success', data: response.data})
     })
-    // tslint:disable-next-line: only-arrow-functions
+    // tslint:disable-next-line: all
     .catch(function(error) {
      logInfo('Error throwing inside Category List Api : ' + error)
      res.send({error: true, message: error})
