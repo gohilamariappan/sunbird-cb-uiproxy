1b8bf2bcc3228ae75599691e44a14de99850d0ff
Ankit <ankitv89@gmail.com>
2022-08-04 23:26:20 +0530

    pre assessment added to zero

--- src/protectedApi_v8/assessment.ts
+++ src/protectedApi_v8/assessment.ts
@@ -83,7 +83,7 @@ assessmentApi.post('/submit/v2', async (req, res) => {
         }
       logInfo('Content has completed the course.' + revisedData)
       // exception for WRAI
-      if (req.body.contentId === 'do_113474390542598144143' || req.body.contentId === 'do_113474390542598144144' ) {
+      if (req.body.contentId === 'do_113474390542598144143' || req.body.contentId === 'do_113474390542598144144' || req.body.contentId === 'do_1135849304134778881218' || req.body.contentId === 'do_1135870134353182721225' ) {
             response.data.passPercent = 0
       }
       if (response.data.result >= response.data.passPercent) {
