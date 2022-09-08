19072812edbe324043c5a25398f6ec25ea9b92c3
Saurav Verma <saurav.verma@tarento.com>
2022-09-08 12:56:57 +0530

    hierarchy issue fixes

--- src/protectedApi_v8/assessment.ts
+++ src/protectedApi_v8/assessment.ts
@@ -84,8 +84,8 @@ assessmentApi.post('/submit/v2', async (req, res) => {
       logInfo('Content has completed the course.' + revisedData)
       // exception for pre-assessments
        // tslint:disable-next-line: max-line-length
-      const assessmentZeroCases = ['do_113474390542598144143','do_113474390542598144144','do_1135849304134778881218','do_11357406890855628811485','do_11357407983906816011501']
-      if(assessmentZeroCases.indexOf(req.body.contentId)>0){
+      const assessmentZeroCases = ['do_113474390542598144143', 'do_113474390542598144144', 'do_1135849304134778881218', 'do_11357406890855628811485', 'do_11357407983906816011501']
+      if (assessmentZeroCases.indexOf(req.body.contentId) > 0) {
         response.data.passPercent = 0
       }
       if (response.data.result >= response.data.passPercent) {
