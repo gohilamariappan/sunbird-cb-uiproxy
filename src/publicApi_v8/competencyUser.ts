8f31cb77999d68cdfd43f921f5c15c8c8f542ea7
Sumit Bajaj <sumit.bajaj@tarento.com>
2022-10-18 12:50:17 +0530

    Competency APIs added

---
+++ src/publicApi_v8/competencyUser.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { Router } from "express";
+import _ from "lodash";
+import { CONSTANTS } from "../utils/env";
+import { logError, logInfo } from "../utils/logger";
+const API_END_POINTS = {
+  COMPETENCY_USER: `${CONSTANTS.COMPETENCY_API_BASE}/api/user`,
+};
+const COMPETENCY_USER_FAIL = "Sorry ! Data is not received in competency.";
+export const publicCompetencyUser = Router();
+
+publicCompetencyUser.get("/", async (req, res) => {
+  try {
+    const response = await axios({
+      method: "GET",
+      url: API_END_POINTS.COMPETENCY_USER,
+    });
+    if (response.data.responseCode === "OK") {
+      logInfo("Check re body >> " + req);
+      logInfo("Log of competency user :");
+      res.status(response.status).send(response.data.result);
+    } else {
+      throw new Error(
+        _.get(response.data, "params.errmsg") ||
+          _.get(response.data, "params.err")
+      );
+    }
+  } catch (error) {
+    logError("Error in competency user  >>>>>>" + error);
+    res.status(500).send({
+      message: COMPETENCY_USER_FAIL,
+      status: "failed",
+    });
+  }
+});
