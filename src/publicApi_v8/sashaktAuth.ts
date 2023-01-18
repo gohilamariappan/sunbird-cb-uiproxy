e382537c6d6400f1ea411a5e74b7952e62efcb24
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-01-18 15:21:02 +0530

    email search fix

--- src/publicApi_v8/sashaktAuth.ts
+++ src/publicApi_v8/sashaktAuth.ts
@@ -1,100 +1,100 @@
-import axios from "axios";
-import express from "express";
-import jwt_decode from "jwt-decode";
-import _ from "lodash";
-import qs from "querystring";
-import { axiosRequestConfig } from "../configs/request.config";
-import { CONSTANTS } from "../utils/env";
-import { logError, logInfo } from "../utils/logger";
-import { getCurrentUserRoles } from "./rolePermission";
+import axios from 'axios'
+import express from 'express'
+import jwt_decode from 'jwt-decode'
+import _ from 'lodash'
+import qs from 'querystring'
+import { axiosRequestConfig } from '../configs/request.config'
+import { CONSTANTS } from '../utils/env'
+import { logError, logInfo } from '../utils/logger'
+import { getCurrentUserRoles } from './rolePermission'
 
 const AUTH_FAIL =
-  "Authentication failed ! Please check credentials and try again.";
+  'Authentication failed ! Please check credentials and try again.'
 const API_END_POINTS = {
   fetchUserByEmail: `${CONSTANTS.KONG_API_BASE}/user/v1/exists/email/`,
   fetchUserByMobileNo: `${CONSTANTS.KONG_API_BASE}/user/v1/exists/phone/`,
   generateToken: `${CONSTANTS.HTTPS_HOST}/auth/realms/sunbird/protocol/openid-connect/token`,
   sashaktUserDetailsUrl: `${CONSTANTS.SASHAKT_USER_DETAILS_URL}`,
-};
-export const sashakt = express.Router();
+}
+export const sashakt = express.Router()
 // tslint:disable-next-line: no-any
-sashakt.get("/login", async (req: any, res) => {
-  logInfo("Entered into sashakt route");
-  const courseId = req.query.moduleId;
-  const host = req.get("host");
-  let resRedirectUrl = `https://sphere.aastrika.org/app/toc/${courseId}/overview?primaryCategory=Course`;
+sashakt.get('/login', async (req: any, res) => {
+  logInfo('Entered into sashakt route')
+  const courseId = req.query.moduleId
+  const host = req.get('host')
+  let resRedirectUrl = `https://sphere.aastrika.org/app/toc/${courseId}/overview?primaryCategory=Course`
   try {
-    const sashaktToken = "Bearer " + req.query.token;
+    const sashaktToken = 'Bearer ' + req.query.token
     const userDetailResponseFromShashakt = await axios({
       ...axiosRequestConfig,
       headers: {
         Authorization: sashaktToken,
       },
-      method: "POST",
+      method: 'POST',
       url: API_END_POINTS.sashaktUserDetailsUrl,
-    });
-    const userEmail = userDetailResponseFromShashakt.data.userDetails[0].email;
-    logInfo("User details from shashakt", userEmail);
+    })
+    const userEmail = userDetailResponseFromShashakt.data.userDetails[0].email
+    logInfo('User details from shashakt', userEmail)
     if (!userDetailResponseFromShashakt.data.userDetails) {
-      logInfo("User details not present in e shashakt");
+      logInfo('User details not present in e shashakt')
     }
     const resultEmail = await fetchUserBymobileorEmail(
-      userDetailResponseFromShashakt.data.email,
-      "email"
-    );
-    logInfo("User details sunbird", resultEmail);
+      userDetailResponseFromShashakt.data.userDetails[0].email,
+      'email'
+    )
+    logInfo('User details sunbird', resultEmail)
     if (!resultEmail) {
-      logInfo("Received error from user search sunbird. ");
+      logInfo('Received error from user search sunbird. ')
     }
     const encodedData = qs.stringify({
-      client_id: "eShashakt",
+      client_id: 'eShashakt',
       client_secret: `${CONSTANTS.KEYCLOAK_CLIENT_SECRET_SASHAKT}`,
-      grant_type: "password",
-      scope: "offline_access",
+      grant_type: 'password',
+      scope: 'offline_access',
       username: userEmail,
-    });
-    logInfo("Entered into authorization part." + encodedData);
-    logInfo(userEmail, "useremail..");
+    })
+    logInfo('Entered into authorization part.' + encodedData)
+    logInfo(userEmail, 'useremail..')
     const authTokenResponse = await axios({
       ...axiosRequestConfig,
       data: encodedData,
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-      method: "POST",
+      method: 'POST',
       url: API_END_POINTS.generateToken,
-    });
+    })
     if (authTokenResponse.data) {
-      const accessToken = authTokenResponse.data.access_token;
+      const accessToken = authTokenResponse.data.access_token
       // tslint:disable-next-line: no-any
-      const decodedToken: any = jwt_decode(accessToken);
-      const decodedTokenArray = decodedToken.sub.split(":");
-      const userId = decodedTokenArray[decodedTokenArray.length - 1];
-      req.session.userId = userId;
-      logInfo(userId, "userid......................");
+      const decodedToken: any = jwt_decode(accessToken)
+      const decodedTokenArray = decodedToken.sub.split(':')
+      const userId = decodedTokenArray[decodedTokenArray.length - 1]
+      req.session.userId = userId
+      logInfo(userId, 'userid......................')
       req.kauth = {
         grant: {
           access_token: { content: decodedToken, token: accessToken },
         },
-      };
+      }
       req.session.grant = {
         access_token: { content: decodedToken, token: accessToken },
-      };
-      logInfo("Success ! Entered into usertokenResponse..");
-      await getCurrentUserRoles(req, accessToken);
+      }
+      logInfo('Success ! Entered into usertokenResponse..')
+      await getCurrentUserRoles(req, accessToken)
     } else {
       res.status(302).json({
         msg: AUTH_FAIL,
-        status: "error",
-      });
+        status: 'error',
+      })
     }
   } catch (err) {
-    logError("Failed to process callback API.. error: " + JSON.stringify(err));
-    resRedirectUrl = `https://${host}/public/home`;
+    logError('Failed to process callback API.. error: ' + JSON.stringify(err))
+    resRedirectUrl = `https://${host}/public/home`
   }
-  logInfo(resRedirectUrl, "redirectUrl");
-  res.redirect(resRedirectUrl);
-});
+  logInfo(resRedirectUrl, 'redirectUrl')
+  res.redirect(resRedirectUrl)
+})
 
 const fetchUserBymobileorEmail = async (
   searchValue: string,
@@ -106,22 +106,22 @@ const fetchUserBymobileorEmail = async (
       headers: {
         Authorization: CONSTANTS.SB_API_KEY,
       },
-      method: "GET",
+      method: 'GET',
       url:
-        searchType === "email"
+        searchType === 'email'
           ? API_END_POINTS.fetchUserByEmail + searchValue
           : API_END_POINTS.fetchUserByMobileNo + searchValue,
-    });
-    logInfo("Response Data in JSON :", JSON.stringify(response.data));
-    logInfo("Response Data in Success :", response.data.responseCode);
-    if (response.data.responseCode === "OK") {
+    })
+    logInfo('Response Data in JSON :', JSON.stringify(response.data))
+    logInfo('Response Data in Success :', response.data.responseCode)
+    if (response.data.responseCode === 'OK') {
       logInfo(
-        "Response result.exists :",
-        _.get(response, "data.result.exists")
-      );
-      return _.get(response, "data.result.exists");
+        'Response result.exists :',
+        _.get(response, 'data.result.exists')
+      )
+      return _.get(response, 'data.result.exists')
     }
   } catch (err) {
-    logError("fetchUserByMobile  failed");
+    logError('fetchUserByMobile  failed')
   }
-};
+}
