f0c4fc706dfe52df9bf4a86943fdd67c85a5e513
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-02-06 15:39:30 +0530

    lint fix

--- src/utils/jumbler.ts
+++ src/utils/jumbler.ts
@@ -1,48 +1,49 @@
-import axios from "axios";
-import _ from "lodash";
-import { CONSTANTS } from "./env";
-import { logInfo } from "./logger";
-const S3_BUCKET_URL = `${CONSTANTS.S3_BUCKET_URL}`;
+import axios from 'axios'
+import _ from 'lodash'
+import { CONSTANTS } from './env'
+import { logInfo } from './logger'
+const S3_BUCKET_URL = `${CONSTANTS.S3_BUCKET_URL}`
 
 export async function jumbler(path: string) {
-  const sunbirdUrl = S3_BUCKET_URL + path;
+  const sunbirdUrl = S3_BUCKET_URL + path
   return axios({
-    method: "get",
+    method: 'get',
     url: sunbirdUrl,
   }).then((response) => {
     const randomCount =
-      response.data.randomCount || response.data.questions.length;
-    logInfo("Success IN Getting Assessment JSON >>>>>>>>>>>" + response);
+      response.data.randomCount || response.data.questions.length
+    logInfo('Success IN Getting Assessment JSON >>>>>>>>>>>' + response)
     const questionArray = _.sampleSize(response.data.questions, randomCount)
       .map(falseCreator)
-      .map(indexEraser);
+      .map(indexEraser)
     const questionObject = {
       isAssessment: true,
       questions: questionArray,
-      randomCount: "20",
+      randomCount: '20',
       timeLimit: response.data.timeLimit,
-    };
-    logInfo("Question format...." + questionObject);
-    i = 0;
-    return questionObject;
-  });
+    }
+    logInfo('Question format....' + questionObject)
+    i = 0
+    return questionObject
+  })
 }
-let i = 0;
+let i = 0
 // tslint:disable-next-line: no-any
 const falseCreator = (nums: any) => {
   for (const value of nums.options) {
-    value.isCorrect = false;
+    value.isCorrect = false
   }
-  return nums;
-};
+  return nums
+}
+// tslint:disable-next-line: no-any
 const indexEraser = (value: any) => {
   value.question =
     i +
-    "." +
+    '.' +
     value.question.slice(
-      value.question.indexOf(";") + 1,
+      value.question.indexOf(';') + 1,
       value.question.length
-    );
-  i++;
-  return value;
-};
+    )
+  i++
+  return value
+}
