865012681748cceda4bbb9c9e443676c77d3228c
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-04-05 16:19:03 +0530

    facets added

--- src/publicApi_v8/publicSearch.ts
+++ src/publicApi_v8/publicSearch.ts
@@ -1,24 +1,24 @@
-import axios from 'axios'
-import { Router } from 'express'
-import { Client } from 'pg'
-import { CONSTANTS } from '../utils/env'
-import { logInfo } from '../utils/logger'
+import axios from "axios";
+import { Router } from "express";
+import { Client } from "pg";
+import { CONSTANTS } from "../utils/env";
+import { logInfo } from "../utils/logger";
 
-export const publicSearch = Router()
-const unknownError = 'Failed due to unknown reason'
+export const publicSearch = Router();
+const unknownError = "Failed due to unknown reason";
 
 const API_END_POINTS = {
   search: `${CONSTANTS.HTTPS_HOST}/apis/public/v8/publicContent/v1/search`,
-}
+};
 const elasticSearchConnectionDetails = {
   database: CONSTANTS.POSTGRES_DATABASE,
   host: CONSTANTS.POSTGRES_HOST,
   password: CONSTANTS.POSTGRES_PASSWORD,
   port: CONSTANTS.POSTGRES_PORT,
   user: CONSTANTS.POSTGRES_USER,
-}
+};
 
-publicSearch.post('/getCourses', async (request, response) => {
+publicSearch.post("/getCourses", async (request, response) => {
   try {
     const client = new Client({
       database: elasticSearchConnectionDetails.database,
@@ -26,62 +26,64 @@ publicSearch.post('/getCourses', async (request, response) => {
       password: elasticSearchConnectionDetails.password,
       port: Number(elasticSearchConnectionDetails.port),
       user: elasticSearchConnectionDetails.user,
-    })
-    const courseSearchRequestData = request.body.data.value
+    });
+    const courseSearchRequestData = request.body.data.value;
+    logInfo(courseSearchRequestData, "Request data");
     const courseSearchPrimaryData = JSON.stringify({
       request: {
         facets: [
-          'learningMode',
-          'duration',
-          'complexityLevel',
-          'catalogPaths',
-          'sourceShortName',
-          'region',
-          'concepts',
-          'lastUpdatedOn',
+          "learningMode",
+          "duration",
+          "complexityLevel",
+          "catalogPaths",
+          "sourceShortName",
+          "region",
+          "concepts",
+          "lastUpdatedOn",
         ],
         fields: [],
         filters: {
-          contentType: ['Course'],
-          status: ['Live'],
+          contentType: ["Course"],
+          status: ["Live"],
         },
         query: `${courseSearchRequestData}`,
         sort_by: {
-          lastUpdatedOn: '',
+          lastUpdatedOn: "",
         },
       },
-    })
+    });
     const esResponsePrimaryCourses = await axios({
       data: courseSearchPrimaryData,
       headers: {
         // tslint:disable-next-line:max-line-length
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-        org: 'aastar',
-        rootorg: 'aastar',
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        org: "aastar",
+        rootorg: "aastar",
       },
-      method: 'post',
+      method: "post",
       url: API_END_POINTS.search,
-    })
-    const courseDataPrimary = esResponsePrimaryCourses.data.result.content
-    logInfo('Coursedata for primary courses length', courseDataPrimary.length)
+    });
+    const courseDataPrimary = esResponsePrimaryCourses.data.result.content;
+    const facetsData = esResponsePrimaryCourses.data.result.facets;
+    logInfo("Coursedata for primary courses length", courseDataPrimary.length);
 
     try {
-      client.connect()
-      let finalConcatenatedData = []
+      client.connect();
+      let finalConcatenatedData = [];
       client.query(
         `SELECT id FROM public.data_node where type='Competency' and name LIKE  '%${courseSearchRequestData}%'`,
         // tslint:disable-next-line: no-any
         async (err: any, res: any) => {
-          logInfo(err)
+          logInfo(err);
           // tslint:disable-next-line: no-any
-          const postgresResponseData = res.rows.map((val: any) => val.id)
-          let courseDataSecondary = []
+          const postgresResponseData = res.rows.map((val: any) => val.id);
+          let courseDataSecondary = [];
           if (postgresResponseData.length > 0) {
-            const elasticSearchData = []
+            const elasticSearchData = [];
             for (const postgresResponse of postgresResponseData) {
               for (const value of [1, 2, 3, 4, 5]) {
-                elasticSearchData.push(`${postgresResponse}-${value}`)
+                elasticSearchData.push(`${postgresResponse}-${value}`);
               }
             }
 
@@ -89,52 +91,54 @@ publicSearch.post('/getCourses', async (request, response) => {
               request: {
                 filters: {
                   competencySearch: elasticSearchData,
-                  contentType: ['Course'],
-                  primaryCategory: ['Course'],
-                  status: ['Live'],
+                  contentType: ["Course"],
+                  primaryCategory: ["Course"],
+                  status: ["Live"],
                 },
-                sort_by: { lastUpdatedOn: 'desc' },
+                sort_by: { lastUpdatedOn: "desc" },
               },
-              sort: [{ lastUpdatedOn: 'desc' }],
-            }
+              sort: [{ lastUpdatedOn: "desc" }],
+            };
             const elasticSearchResponseSecond = await axios({
               data: courseSearchSecondaryData,
               headers: {
                 // tslint:disable-next-line:max-line-length
-                Accept: 'application/json, text/plain, */*',
-                'Content-Type': 'application/json',
-                org: 'aastar',
-                rootorg: 'aastar',
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+                org: "aastar",
+                rootorg: "aastar",
               },
-              method: 'post',
+              method: "post",
               url: API_END_POINTS.search,
-            })
-            client.end()
+            });
+            client.end();
             courseDataSecondary =
-              elasticSearchResponseSecond.data.result.content
+              elasticSearchResponseSecond.data.result.content;
+
             logInfo(
-              'Coursedata for secondary courses length',
+              "Coursedata for secondary courses length",
               JSON.stringify(courseDataSecondary.length)
-            )
+            );
           }
-          finalConcatenatedData = courseDataPrimary.concat(courseDataSecondary)
+          finalConcatenatedData = courseDataPrimary.concat(courseDataSecondary);
           response.status(200).json({
             result: {
               content: finalConcatenatedData,
+              factes: facetsData,
             },
-          })
+          });
         }
-      )
+      );
     } catch (error) {
       response.status(400).json({
-        message: 'Error while connecting postgres',
-      })
+        message: "Error while connecting postgres",
+      });
     }
   } catch (err) {
     response.status((err && err.response && err.response.status) || 500).send(
       (err && err.response && err.response.data) || {
         error: unknownError,
       }
-    )
+    );
   }
-})
+});
