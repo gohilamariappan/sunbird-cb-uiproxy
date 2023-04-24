4910d00cc43afb723c0f43e90f567c0a8c63296d
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-04-24 10:55:47 +0530

    jumbler randdom count fixed

--- src/utils/jumbler.ts
+++ src/utils/jumbler.ts
@@ -1,36 +1,36 @@
-import axios from 'axios'
-import _ from 'lodash'
-import { CONSTANTS } from './env'
-import { logInfo } from './logger'
-const S3_BUCKET_URL = `${CONSTANTS.S3_BUCKET_URL}`
+import axios from "axios";
+import _ from "lodash";
+import { CONSTANTS } from "./env";
+import { logInfo } from "./logger";
+const S3_BUCKET_URL = `${CONSTANTS.S3_BUCKET_URL}`;
 
 export async function jumbler(path: string) {
-  const sunbirdUrl = S3_BUCKET_URL + path
+  const sunbirdUrl = S3_BUCKET_URL + path;
   return axios({
-    method: 'get',
+    method: "get",
     url: sunbirdUrl,
   }).then((response) => {
     const randomCount =
-      response.data.randomCount || response.data.questions.length
-    logInfo('Success IN Getting Assessment JSON >>>>>>>>>>>' + response)
+      response.data.randomCount || response.data.questions.length;
+    logInfo("Success IN Getting Assessment JSON >>>>>>>>>>>" + response);
     const questionArray = _.sampleSize(
       response.data.questions,
       randomCount
-    ).map(falseCreator)
+    ).map(falseCreator);
     const questionObject = {
       isAssessment: true,
       questions: questionArray,
-      randomCount: '20',
+      randomCount,
       timeLimit: response.data.timeLimit,
-    }
-    logInfo('Question format....' + questionObject)
-    return questionObject
-  })
+    };
+    logInfo("Question format...." + questionObject);
+    return questionObject;
+  });
 }
 // tslint:disable-next-line: no-any
 const falseCreator = (nums: any) => {
   for (const value of nums.options) {
-    value.isCorrect = false
+    value.isCorrect = false;
   }
-  return nums
-}
+  return nums;
+};
